2ca169e1a6dc507e7c57a2c993c60ba5032ed218
Lucas Rodrigues <lucasdantasrodrigues847@gmail.com>
2024-02-21 16:11:29 -0300

    globalstyle adicionado

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+padding: 0;
+margin: 0;
+list-style: none;
+font-family: sans-serif;
+}
+
+button{
+    cursor: pointer;
+}
+
+
+a{
+    cursor: pointer;
+}
+
+:root{
+    --color-primary: #27ae60;
+    --color-primary-50: #93d7af;
+    --color-secondary: #eb5757;
+    --black: #000;
+    --White: #fff
+    --grey-100: #333333;
+    --grey-50: #828282;
+    --grey-30: #a0a0a0;
+    --grey-20: #E0E0E0;
+    --grey-0: #f5f5f5;
+    --negative: #e60000;
+    --warning: #ffcd07;
+    --sucess: #168821;
+    --information: #155aca
+}
+`
+export default GlobalStyles;
