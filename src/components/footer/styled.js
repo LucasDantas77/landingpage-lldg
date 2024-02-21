dc1012cbbf707e9b5513f47ca8978d5b0d4d5dff
DenisGuedes1 <denisguedesmoraes@gmail.com>
2024-02-21 18:28:46 -0300

    alterations css footer

--- src/components/footer/styled.js
+++ src/components/footer/styled.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 import { IoLogoInstagram } from "react-icons/io";
 import { BiLogoGmail } from "react-icons/bi";
 export const FooterContainer = styled.footer`
-display: flex;
-justify-content: space-around;
-align-items: center;
-text-align: center;
-gap: 20px;
-flex-direction: column;
-height: 70px;
-background-color: #333333;
-max-width: 100vw;
-width: 95vw;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  text-align: center;
+  gap: 20px;
+  flex-direction: column;
+  height: 70px;
+  background-color: #333333;
+  max-width: 100vw;
+  width: 95vw;
 
   p {
     margin: 0;
@@ -23,12 +23,12 @@ width: 95vw;
     display: flex;
     gap: 10px;
     margin-top: 10px;
-    width: 0%;
+    /* width: 0%; Remova ou ajuste conforme necessário */
     height: 50%;
-    margin-left: 50rem;
-}
+    /* margin-left: 50rem; Remova ou ajuste conforme necessário */
   }
-  span{
+
+  span {
     display: flex;
     gap: 25px;
   }
@@ -41,32 +41,20 @@ width: 95vw;
     }
   }
 
-  @media (min-width: 601px) and (max-width: 900px) {
-    /* Estilos específicos para telas médias */
-  }
+  /* Outras regras de mídia conforme necessário */
 
-  @media (min-width: 901px) {
-    /* Estilos específicos para telas grandes */
-  }
 `;
 
 export const IconInstagram = styled(IoLogoInstagram)`
-background: linear-gradient(90deg, white 50%, #8f2828 50%);
+  background: linear-gradient(90deg, white 50%, #8f2828 50%);
   border-radius: 25px;
   width: 40px;
   height: 30px;
+`;
 
-
-}
-
-
-`
 export const IconGmail = styled(BiLogoGmail)`
-background: linear-gradient(90deg, white 50%, #8f2828 50%);
+  background: linear-gradient(90deg, white 50%, #8f2828 50%);
   border-radius: 25px;
   width: 40px;
   height: 30px;
-
-}
-
-`
+`;
