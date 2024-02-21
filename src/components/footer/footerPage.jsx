28a3cc99342c3a5c0bccb9ce20beec98727eb877
DenisGuedes1 <denisguedesmoraes@gmail.com>
2024-02-21 16:08:04 -0300

    add footter page

--- src/components/footer/footerPage.jsx
+++ src/components/footer/footerPage.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from "react";
+import { IoLogoInstagram } from "react-icons/io";
+import { BiLogoGmail } from "react-icons/bi";
+import { MdAlternateEmail } from "react-icons/md";
+
 const FootterPage = () => {
-    return(
-        <footer>
-            
-        </footer>
-    )
-  };
-  
-  export default FootterPage;
+  return (
+    <footer>
+      <p>
+        LLDG
+        <MdAlternateEmail />
+        2024
+      </p>
+
+      <div>
+        <IoLogoInstagram />
+        <BiLogoGmail />
+      </div>
+    </footer>
+  );
+};
+
+export default FootterPage;
