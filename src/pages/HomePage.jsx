472cefc8c9817cdebc913dccc0df34245c5a1f42
Lucas Rodrigues <lucasdantasrodrigues847@gmail.com>
2024-02-27 19:41:38 -0300

    add logo na aba

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,14 +4,14 @@ import { MainPage } from "./style";
 import { Introducao } from "../components/Introducao/Intro";
 import { Carousel } from "../components/carrosel/carrosel";
 
-const images = [
-  "./src/assets/template1.jpg",
-  "./src/assets/template2.png",
-  "./src/assets/template3.png",
-  "./src/assets/template4.png",
-  "./src/assets/template5.png",
-];
 const HomePage = () => {
+  const images = [
+    "./src/assets/template1.jpg",
+    "./src/assets/template2.png",
+    "./src/assets/template3.png",
+    "./src/assets/template4.png",
+    "./src/assets/template5.png",
+  ];
   return (
     <MainPage>
       <HeaderPage />
