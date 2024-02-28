0f75938f8cefcb65755745d5f085fb9543482254
Lucas Rodrigues <lucasdantasrodrigues847@gmail.com>
2024-02-28 18:31:03 -0300

    secao sobre finalizada com video

--- src/components/sobre/index.jsx
+++ src/components/sobre/index.jsx
@@ -1,12 +1,41 @@
+import React, { useEffect, useRef, useState } from "react";
+import { DivSobre, SectionSobre } from "./style";
+
 export const Sobre = () => {
+  const videoRef1 = useRef(null);
+  const videoRef2 = useRef(null);
+
+  const handleStartPlayback = (videoRef) => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
   return (
-    <section>
-      <h2>Sobre nós</h2>
-      <div>
-        <video width="640" height="360" controls>
-          <source src="caminho/do/seu/video.mp4" type="video/mp4" />
-        </video>
-      </div>
-    </section>
+    <SectionSobre>
+      <p>Sobre-Nós</p>
+      <DivSobre>
+        <div>
+          <video ref={videoRef1} controls autoPlay loop>
+            <source
+              src="./src/assets/Lucas Dantas Rodrigues.mp4"
+              type="video/mp4"
+            />
+          </video>
+          <button onClick={() => handleStartPlayback(videoRef1)}>
+            Iniciar Reprodução do Vídeo 1
+          </button>
+        </div>
+        <div>
+          {" "}
+          <video ref={videoRef2} controls autoPlay loop>
+            <source src="./src/assets/Denis.mp4" type="video/mp4" />
+          </video>
+          <button onClick={() => handleStartPlayback(videoRef2)}>
+            Iniciar Reprodução do Vídeo 2
+          </button>
+        </div>
+      </DivSobre>
+    </SectionSobre>
   );
 };
