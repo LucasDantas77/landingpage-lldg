7250118b878ce6c90a4a937096b39e189d0559f9
Lucas Rodrigues <lucasdantasrodrigues847@gmail.com>
2024-02-29 18:54:18 -0300

    font size aumentada dos btns contatos

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,28 +1,34 @@
 import React from "react";
 
-import { ConteinerDiv, IconWpp, IconEmail, IconInstagram,H2text, DivIcons} from "./styled";
+import {
+  ConteinerDiv,
+  IconWpp,
+  IconEmail,
+  IconInstagram,
+  H2text,
+  DivIcons,
+} from "./styled";
 
 export const Contacts = () => {
   return (
     <>
       <ConteinerDiv>
-      <H2text>Entre em contato por:</H2text>
+        <H2text>Entre em contato por:</H2text>
         <DivIcons>
+          <a href="https://api.whatsapp.com/send?phone=12978131357">
+            <IconWpp style={{ fontSize: "40px" }} />
+          </a>
 
-        <a href="https://api.whatsapp.com/send?phone=12978131357">
-          <IconWpp />
-        </a>
-
-        <a
-          href="https://www.instagram.com/lldgcontato/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <IconInstagram />
-        </a>
-        <a href="mailto:lldgcontato@gmail.com">
-          <IconEmail />
-        </a>
+          <a
+            href="https://www.instagram.com/lldgcontato/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconInstagram style={{ fontSize: "40px" }} />
+          </a>
+          <a href="mailto:lldgcontato@gmail.com">
+            <IconEmail style={{ fontSize: "40px" }} />
+          </a>
         </DivIcons>
       </ConteinerDiv>
     </>
